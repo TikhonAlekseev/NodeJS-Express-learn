d54a8838c9840bffc13b4b3e5f55695dbc26aedd
Tikhon <alexeev.tix@yandex.ru>
2022-12-08 23:59:47 +0300

    created room service

---
+++ service/RoomService.js
@@ -0,0 +1,27 @@
+const Room = require("../models/room");
+
+class RoomService {
+
+    async getRooms() {
+        const rooms = await Room.find();
+        const roomsFormats = rooms.map(room =>( { id:room._id.toString(), name:room.name }))
+        return roomsFormats
+    }
+
+    async createRoom(name) {
+        if(!name){
+            throw new Error({message:"parameter 'name' is invalid"})
+        }
+
+        await Room.create( { name } )
+    }
+
+    async deleteRoom(id) {
+        if(!id){
+            throw new Error({ message:"parameter 'id' is not exists" } )
+        }
+        await Room.findByIdAndDelete(id)
+    }
+}
+
+module.exports = new RoomService();
