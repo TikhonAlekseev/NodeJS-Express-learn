d2f36c7de84d88c1d536d37e2853d1ed8f147bb3
tikhon <alexeev.tix@yandex.ru>
2023-01-31 23:42:22 +0300

    created dto/message-dto.js

---
+++ dto/message-dto.js
@@ -0,0 +1,8 @@
+module.exports = class MessageDto {
+    constructor(model){
+        this.roomId = model.roomId;
+        this.text = model.text;
+        this.id = model._id;
+        this.user = model.user;
+    }
+}
